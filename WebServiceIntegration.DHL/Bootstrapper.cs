ca80a40d1a60eddc1901b6ed787094d04b7d4240
Marc Woolfson <marc.woolfson@barrettsteel.com>
2018-01-26 09:54:45 +0000

    Added INSECURE password for access to /_Nancy/ diagnostics tool for request tracing

---
+++ WebServiceIntegration.DHL/Bootstrapper.cs
@@ -0,0 +1,14 @@
+﻿using Nancy;
+using Nancy.Bootstrapper;
+using Nancy.Diagnostics;
+
+namespace WebServiceIntegration.DHL
+{
+    public class Bootstrapper : DefaultNancyBootstrapper
+    {
+        protected override DiagnosticsConfiguration DiagnosticsConfiguration
+        {
+            get { return new DiagnosticsConfiguration() { Password = "password" }; }
+        }
+    }
+}
---
+++ WebServiceIntegration.Fedex/Bootstrapper.cs
@@ -0,0 +1,14 @@
+﻿using Nancy;
+using Nancy.Bootstrapper;
+using Nancy.Diagnostics;
+
+namespace WebServiceIntegration.Fedex
+{
+    public class Bootstrapper : DefaultNancyBootstrapper
+    {
+        protected override DiagnosticsConfiguration DiagnosticsConfiguration
+        {
+            get { return new DiagnosticsConfiguration() { Password = "password" }; }
+        }
+    }
+}
